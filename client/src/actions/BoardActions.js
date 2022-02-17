507bb30495989fae0732512fe0f26cb831c11e3f
Emily Ma <em.kayama@gmail.com>
2022-02-17 07:11:53 -0500

    add initial files

---
+++ client/src/actions/BoardActions.js
@@ -0,0 +1,38 @@
+import apiClient from "../lib/ApiClient";
+import * as types from "../constants/ActionTypes";
+
+export function fetchBoardsRequest() {
+  return { type: types.FETCH_BOARDS_REQUEST };
+}
+
+export function fetchBoardsSuccess(boards) {
+  return { type: types.FETCH_BOARDS_SUCCESS, boards };
+}
+
+export function createBoardRequest() {
+  return { type: types.CREATE_BOARD_REQUEST };
+}
+
+export function createBoardSuccess(board) {
+  return { type: types.CREATE_BOARD_SUCCESS, board: board };
+}
+
+export function fetchBoards() {
+  return function(dispatch) {
+    dispatch(fetchBoardsRequest());
+    apiClient.getBoards(data => dispatch(fetchBoardsSuccess(data.boards)));
+  };
+}
+
+export function createBoard(board, callback) {
+  return function(dispatch) {
+    dispatch(createBoardRequest());
+    apiClient.createBoard(board, data => {
+      dispatch(createBoardSuccess(data.board));
+
+      if (callback) {
+        callback(data.board);
+      }
+    });
+  };
+}
